296adf4ac1cead75a8dd52ee21a7063a42dc8bb4
bardia <bardia.keyvani@gmail.com>
2013-03-13 15:06:27 -0700

    user class commit

---
+++ GotTagged380-AppEngine/src/com/gottagged380/User.java
@@ -0,0 +1,59 @@
+package com.gottagged380;
+import java.util.Date;
+
+import javax.persistence.*;
+
+import com.googlecode.objectify.annotation.Index;
+
+@Entity
+public class User {
+	
+	 @Id
+	 private String name;	
+	 @Index
+	 private String email;
+	 private String deviceRegisterId;
+	 private String deviceType;
+	 private Date dateRegistered;
+	
+	 public User() {
+
+	 }
+
+	 public User(String name, String email, String deviceType, String deviceId) {
+	  this.name = name;
+	  this.email = email;
+	  this.deviceRegisterId = deviceId;	
+	  this.dateRegistered = new Date();
+	 }
+
+	 public String getName() {
+	  return name;
+	 }
+	 
+	 public String getEmail(){
+		 return email;
+	 }
+	 
+	 public String getDeviceRegisterId(){
+		 return deviceRegisterId;
+	 }
+	 
+	 public String getDeviceType(){
+		 return deviceType;
+	 }
+	 
+	 public void setName(String name){
+		 this.name = name;
+	 }
+	 public void setDeviceRegisterId(String id){
+		 this.deviceRegisterId = id;
+	 }
+	 public void setDeviceType(String type){
+		 this.deviceType = type;
+	 }
+	 
+}	
+
+
+
